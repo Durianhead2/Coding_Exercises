2b4c1ffb92aa6590aee656f816220fe44a7c3ce5
Durianhead2 <143659756+Durianhead2@users.noreply.github.com>
2024-06-27 14:19:45 -0500

    Update circle.py

--- circle.py
+++ circle.py
@@ -14,8 +14,3 @@ class Circle:
     def calculatePerimeter(self):
       self.perimeter = 2 * np.pi * self.radius
       return self.perimeter
-
-circle1 = Circle(5)
-
-print(circle1.calculateArea())
-print(circle1.calculatePerimeter())
